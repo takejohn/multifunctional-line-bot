ccef50a315cd8e9755720ec6bb7b52bf8c7da238
takejohn <105504345+takejohn@users.noreply.github.com>
2024-03-15 19:52:34 +0900

    Webサーバーの実装

--- main/index.ts
+++ main/index.ts
@@ -1,6 +1,45 @@
-import { TokenManager } from './token';
+import { middleware } from '@line/bot-sdk';
+import express from 'express';
+import { KeyTokenPair, TokenManager } from './token';
+import { Server } from 'http';
+
+interface Context {
+    server: Server;
+    tokenManager: TokenManager;
+    token: KeyTokenPair;
+}
+
+let closed = false;
 
 export async function main(): Promise<void> {
-    const accessManager = new TokenManager();
-    console.log(await accessManager.issueToken(10));
+    const tokenManager = new TokenManager();
+    const token = await tokenManager.issueToken();
+    const lineMiddleware = middleware({
+        channelAccessToken: token.accessToken,
+        channelSecret: await tokenManager.getChannelSecret(),
+    });
+    const app = express();
+    app.post('/webhook', lineMiddleware);
+    const port = 45110;
+    const server = app.listen(port);
+    const context: Context = { server, tokenManager, token };
+    console.log(`ポート ${port} で待ち受けを開始しました`);
+    await new Promise((resolve, reject) => {
+        process.on('SIGINT', () => close(context).then(resolve, reject));
+    });
+}
+
+async function close({ server, tokenManager, token }: Context): Promise<void> {
+    await tokenManager.revokeToken(token);
+    console.log('サーバーを終了します');
+    await new Promise<void>((resolve, reject) => {
+        server.close((err) => {
+            if (err == null) {
+                console.log('サーバーを終了しました');
+                resolve();
+            } else {
+                reject(err);
+            }
+        });
+    });
 }
--- main/token.ts
+++ main/token.ts
@@ -2,8 +2,11 @@ import { channelAccessToken } from '@line/bot-sdk';
 import { database, token } from 'common';
 
 export interface TokenManagerOptions {
-    /** jwtExpirationTime 秒単位での JWT の有効期間 (最大 1800 秒) */
+    /** 秒単位での JWT の有効期間 (最大 1800 秒) */
     jwtExpirationTime?: number;
+
+    /** 秒単位でのチャネルアクセストークンの有効期間 (最大 30 日) */
+    tokenExpirationTime?: number;
 
     apiBaseURL?: string;
 }
@@ -16,39 +19,97 @@ async function getStaticChannelInfo() {
     return info;
 }
 
+export interface KeyTokenPair {
+    keyId: string;
+    accessToken: string;
+}
+
 export class TokenManager {
     private readonly staticChannelInfoPromise = getStaticChannelInfo();
 
     private readonly jwtExpirationTime: number;
 
+    private readonly tokenExpirationTime: number;
+
     private readonly channelAccessTokenClient: channelAccessToken.ChannelAccessTokenClient;
 
     constructor(options?: TokenManagerOptions) {
         this.jwtExpirationTime = options?.jwtExpirationTime ?? 10;
+        this.tokenExpirationTime =
+            options?.tokenExpirationTime ?? 60 * 60 * 24 * 2;
         this.channelAccessTokenClient =
             new channelAccessToken.ChannelAccessTokenClient({
                 baseURL: options?.apiBaseURL ?? 'https://api.line.me',
             });
     }
 
-    async issueToken(
-        expirationTime: number,
-    ): Promise<channelAccessToken.IssueChannelAccessTokenResponse> {
+    async getChannelId() {
+        return (await this.staticChannelInfoPromise).channel_id;
+    }
+
+    async getChannelSecret() {
+        return (await this.staticChannelInfoPromise).channel_secret;
+    }
+
+    async issueToken(): Promise<channelAccessToken.IssueChannelAccessTokenResponse> {
+        const res = (await this.channelAccessTokenClient.issueChannelTokenByJWT(
+            'client_credentials',
+            'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+            await this.generateJwt(),
+        )) as object as {
+            access_token: string;
+            token_type: 'Bearer';
+            expires_in: number;
+            key_id: string;
+        }; // TODO: open SDK (ChannelAccessTokenClient#issueChannelTokenByJWT) issue
+        return {
+            accessToken: res.access_token,
+            tokenType: res.token_type,
+            expiresIn: res.expires_in,
+            keyId: res.key_id,
+        };
+    }
+
+    async revokeToken(token: KeyTokenPair): Promise<boolean> {
+        const jwt = await this.generateJwt();
+        const client = this.channelAccessTokenClient;
+        const kidsRes = await fetch(
+            'https://api.line.me/oauth2/v2.1/tokens/kid?' +
+                new URLSearchParams({
+                    client_assertion_type:
+                        'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+                    client_assertion: jwt,
+                }),
+        );
+        const data: { kids: string[] } = await kidsRes.json(); // TODO: open SDK (ChannelAccessToken#getAllValidChannelQAccessTokenKeyIds) issue
+        if (!data.kids.includes(token.keyId)) {
+            return false;
+        }
+        const revokeRes = await fetch(
+            'https://api.line.me/oauth2/v2.1/revoke',
+            {
+                method: 'POST',
+                body: new URLSearchParams({
+                    client_id: await this.getChannelId(),
+                    client_secret: await this.getChannelSecret(),
+                    access_token: token.accessToken,
+                }),
+            },
+        ); // TODO: open SDK (ChannelAccessToken#getAllValidChannelQAccessTokenKeyIds) issue
+        return true;
+    }
+
+    private async generateJwt() {
         const { channel_id, private_key, kid } =
             await this.staticChannelInfoPromise;
-        const jwt = await token.generateJwt(
+        return await token.generateJwt(
             channel_id,
             {
                 privateKey: private_key,
                 kid,
             },
             this.jwtExpirationTime,
-            expirationTime,
-        );
-        return await this.channelAccessTokenClient.issueChannelTokenByJWT(
-            'client_credentials',
-            'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
-            jwt,
+            this.tokenExpirationTime,
         );
     }
 }
